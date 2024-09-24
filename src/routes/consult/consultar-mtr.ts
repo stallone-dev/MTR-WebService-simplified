ff1c3ff6a776ba304dcc209ee02de4c7dd7bbb7d
stallone-dev <stallone.developer@gmail.com>
2024-09-24 01:11:42 -0300

    refactor(consult): Documentation and testing of the 'ConsultarDadosDoMTR' route

--- src/routes/consult/consultar-mtr.ts
+++ src/routes/consult/consultar-mtr.ts
@@ -1,33 +1,63 @@
+/*
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 import {
-    MtrWSBaseURL,
+    type MtrWSBaseURL,
     MtrWSRoute,
     type MtrWSType,
 } from "@stallone-dev/types-mtr-web-service";
 import { ApiRequest } from "../../model/api-request.ts";
 
-export { consultarDadosMTR };
+export { consultarDadosDoMTR };
 
-class consultarDadosMTR extends ApiRequest {
-    private token: MtrWSType.auth.token;
-    private mtr: string;
+/** Interface para implementação */
+interface consultMTRConfig {
+    mtrID: string;
+    authToken: MtrWSType.auth.token;
+    API_BASE_URL: MtrWSBaseURL;
+}
 
-    constructor(
-        mtrID: string,
-        authToken: MtrWSType.auth.token,
-        API_BASE_URL: MtrWSBaseURL,
-    ) {
+/**
+ * Módulo de consulta dos dados completos de uma MTR
+ *
+ * @example
+ * ```ts
+ *  import { consultarDadosDoMTR } from "..."
+ *  import { MtrWSBaseURL } from "..."
+ *
+ *  const token = "Bearer _TOKEN_"
+ *  const base_url = MtrWSBaseURL.SINIR;
+ *
+ *  // Preparando a API
+ *  const consult = new consultarDadosDoMTR({
+ *      mtrID: "1234123412",
+ *      authToken: token,
+ *      API_BASE_URL: base_url
+ *  });
+ *
+ *  // Gerando o token de acesso
+ *  const result = await consult.getResult();
+ *  // ==> { * MTR-object * }
+ * ```
+ */
+class consultarDadosDoMTR extends ApiRequest {
+    private readonly token: MtrWSType.auth.token;
+    private readonly mtr: string;
+
+    constructor({ mtrID, authToken, API_BASE_URL }: consultMTRConfig) {
         super(API_BASE_URL, MtrWSRoute.CONSULTAR_MTR);
         this.token = authToken;
         this.mtr = mtrID;
     }
 
     /**
-     * Consultar dados de um MTR
+     * Requisição do dados atuais de um determinado MTR
      */
-    public async getResult() {
+    public async getResult(): Promise<MtrWSType.responseModel.consultarMTR> {
         const req = await this.makeRequest<
-            MtrWSType.requestBody.consultarMTR,
-            MtrWSType.responseBody.consultarMtr
+            MtrWSType.requestModel.consultarMTR,
+            MtrWSType.responseModel.consultarMTR
         >({
             method: "GET",
             pathString: this.mtr,
---
+++ test/routes/consult/consultar-mtr.test.ts
@@ -0,0 +1,91 @@
+/*
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+import {
+    MtrWSBaseURL,
+    type MtrWSType,
+} from "@stallone-dev/types-mtr-web-service";
+import { generateTemporaryToken } from "../../token-generator-for-tests.ts";
+import { afterAll, beforeAll, describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+
+import { consultarDadosDoMTR } from "~route/consult/consultar-mtr.ts";
+
+/*
+    Testes para validação das APIs de consulta do MTR individual
+*/
+
+describe("CONSULT-MTR - Tests", () => {
+    let _TOKEN: MtrWSType.auth.token;
+    let _BASE_URL: MtrWSBaseURL;
+    let _MTR: string;
+
+    beforeAll(async () => {
+        const _env = Deno.env.toObject();
+        const _base = (_env.TEST_BASE_API ?? "SINIR") as "SINIR" | "SIGOR";
+
+        _BASE_URL = MtrWSBaseURL[_base];
+        _TOKEN = await generateTemporaryToken(_BASE_URL);
+        _MTR = _env.TEST_CONSULT_MTR ?? "";
+    });
+
+    afterAll(() => {
+        _TOKEN = "Bearer _";
+    });
+
+    describe("Expected scenario", () => {
+        it("Simple consult MTR", async () => {
+            const consult = new consultarDadosDoMTR({
+                mtrID: _MTR,
+                authToken: _TOKEN,
+                API_BASE_URL: _BASE_URL,
+            });
+            const result = await consult.getResult();
+
+            expect(result).toMatchObject({ manNumero: _MTR });
+        });
+    });
+
+    describe("Invalid scenarios", () => {
+        it("Token", async () => {
+            const token = "Bearer _";
+            const consult = new consultarDadosDoMTR({
+                mtrID: _MTR,
+                authToken: token,
+                API_BASE_URL: _BASE_URL,
+            });
+            const result = consult.getResult();
+
+            await expect(result).rejects.toThrow();
+        });
+
+        it("MTR ID", async () => {
+            const consult = new consultarDadosDoMTR({
+                mtrID: "00000",
+                authToken: _TOKEN,
+                API_BASE_URL: _BASE_URL,
+            });
+            const result = consult.getResult();
+
+            const regex = new RegExp(/Manifesto não encontrado!/);
+            await expect(result).rejects.toThrow(regex);
+        });
+
+        it("Base URL", async () => {
+            const base_url = Deno.env.get("TEST_BASE_API") === "SINIR"
+                ? MtrWSBaseURL.SIGOR
+                : MtrWSBaseURL.SINIR;
+
+            const consult = new consultarDadosDoMTR({
+                mtrID: _MTR,
+                authToken: _TOKEN,
+                API_BASE_URL: base_url,
+            });
+            const result = consult.getResult();
+
+            const regex = new RegExp(/Unexpected end of JSON input/);
+            await expect(result).rejects.toThrow(regex);
+        });
+    });
+});
