4cd9eb58e73ecfaf49b6fc233fdf6dbbde7e4eca
stallone-dev <stallone.developer@gmail.com>
2024-09-26 00:53:39 -0300

    refactor(route): Improve 'consultar-casse-por-seu-codigo' route and chance input to object model

--- src/routes/consult/consultar-mtr-por-seu-codigo.ts
+++ src/routes/consult/consultar-mtr-por-seu-codigo.ts
@@ -9,17 +9,49 @@ import {
 } from "@stallone-dev/types-mtr-web-service";
 import { ApiRequest } from "../../model/api-request.ts";
 
-export { consultarMtrPorSeuCodigo };
+export { consultarMTRPorSeuCodigo };
 
-class consultarMtrPorSeuCodigo extends ApiRequest {
+/** Interface para implementação */
+interface consultMTRPorSeuCodigoConfig {
+    seuCodigo: MtrWSType.requestModel.retornaMTRsPorSeuCodigo;
+    authToken: MtrWSType.auth.token;
+    API_BASE_URL: MtrWSBaseURL;
+}
+
+/**
+ * Módulo de consulta dos dados de um MTR através do "SeuCodigo"
+ * @SIGOR_ONLY
+ *
+ * @example
+ * ```ts
+ *  import { consultarMtrPorSeuCodigo } from "..."
+ *  import { MtrWSBaseURL } from "..."
+ *
+ *  const token = "Bearer _TOKEN_"
+ *  const base_url = MtrWSBaseURL.SINIR;
+ *  const seu_codigo = { seuCodigo: "1234", suaUnidade: "1234" }
+ *
+ *  // Preparando a API
+ *  const consult = new consultarMtrPorSeuCodigo({
+ *      seuCodigo: seu_codigo,
+ *      authToken: token,
+ *      API_BASE_URL: base_url
+ *  });
+ *
+ *  // Capturando o resultado
+ *  const result = await consult.getResult();
+ *  // ==> { * MTR-object * }
+ * ```
+ */
+class consultarMTRPorSeuCodigo extends ApiRequest {
     private token: MtrWSType.auth.token;
     private seu_codigo: MtrWSType.requestModel.retornaMTRsPorSeuCodigo;
 
-    constructor(
-        seuCodigo: MtrWSType.requestModel.retornaMTRsPorSeuCodigo,
-        authToken: MtrWSType.auth.token,
-        API_BASE_URL: MtrWSBaseURL,
-    ) {
+    constructor({
+        seuCodigo,
+        authToken,
+        API_BASE_URL,
+    }: consultMTRPorSeuCodigoConfig) {
         super(API_BASE_URL, MtrWSRoute.CONSULTAR_MTR_POR_SEU_CODIGO);
         this.token = authToken;
         this.seu_codigo = seuCodigo;
---
+++ test/routes/consult/consultar-mtr-por-seu-codigo.test.ts
@@ -0,0 +1,115 @@
+/*
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+import {
+    MtrWSBaseURL,
+    type MtrWSType,
+} from "@stallone-dev/types-mtr-web-service";
+import { generateTemporaryToken } from "../../token-generator-for-tests.ts";
+import { afterAll, beforeAll, describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+
+import { consultarMTRPorSeuCodigo } from "~route/consult/consultar-mtr-por-seu-codigo.ts";
+
+/*
+    Testes para validação da API de consulta de MTR por "SeuCodigo"
+*/
+
+describe("CONSULT-MTR-POR-SEU-CODIGO - Tests", () => {
+    /** API ainda não implantada no sistema SINIR */
+    const _ignore = Deno.env.get("TEST_BASE_API") === "SINIR";
+
+    let _base: "SINIR" | "SIGOR";
+    let _TOKEN: MtrWSType.auth.token;
+    let _BASE_URL: MtrWSBaseURL;
+    let _SEUCODIGO: MtrWSType.requestModel.retornaMTRsPorSeuCodigo;
+
+    beforeAll(async () => {
+        const _env = Deno.env.toObject();
+        _base = (_env.TEST_BASE_API ?? "SINIR") as "SINIR" | "SIGOR";
+        _BASE_URL = MtrWSBaseURL[_base];
+        _TOKEN = await generateTemporaryToken(_BASE_URL);
+        _SEUCODIGO = {
+            seuCodigo: _env.TEST_CONSULT_SEUCODIGO,
+            suaUnidade: _env.TEST_CONSULT_SUAUNIDADE,
+        };
+    });
+
+    afterAll(() => {
+        _TOKEN = "Bearer _";
+    });
+
+    describe("Expected scenario", { ignore: _ignore }, () => {
+        it("Simple consult MTR", async () => {
+            const consult = new consultarMTRPorSeuCodigo({
+                seuCodigo: _SEUCODIGO,
+                authToken: _TOKEN,
+                API_BASE_URL: _BASE_URL,
+            });
+            const result = await consult.getResult();
+
+            expect(result).toContain("manNumero");
+        });
+    });
+
+    describe("Invalid scenarios", { ignore: _ignore }, () => {
+        it("Token", async () => {
+            const token = "Bearer _";
+            const consult = new consultarMTRPorSeuCodigo({
+                seuCodigo: _SEUCODIGO,
+                authToken: token,
+                API_BASE_URL: _BASE_URL,
+            });
+            const result = consult.getResult();
+
+            await expect(result).rejects.toThrow();
+        });
+
+        it("SeuCodigo", async () => {
+            const consult = new consultarMTRPorSeuCodigo({
+                seuCodigo: {
+                    seuCodigo: "11",
+                    suaUnidade: Deno.env.get("TEST_CONSULT_SUAUNIDADE") ?? "",
+                },
+                authToken: _TOKEN,
+                API_BASE_URL: _BASE_URL,
+            });
+            const result = consult.getResult();
+
+            const regex = new RegExp(/Manifesto não encontrado!/);
+            await expect(result).rejects.toThrow(regex);
+        });
+
+        it("SuaUnidade", async () => {
+            const consult = new consultarMTRPorSeuCodigo({
+                seuCodigo: {
+                    seuCodigo: Deno.env.get("TEST_CONSULT_SEUCODIGO") ?? "",
+                    suaUnidade: "00",
+                },
+                authToken: _TOKEN,
+                API_BASE_URL: _BASE_URL,
+            });
+            const result = consult.getResult();
+
+            const regex = new RegExp(/Manifesto não encontrado!/);
+            await expect(result).rejects.toThrow(regex);
+        });
+
+        it("Base URL", async () => {
+            const base_url = Deno.env.get("TEST_BASE_API") === "SINIR"
+                ? MtrWSBaseURL.SIGOR
+                : MtrWSBaseURL.SINIR;
+
+            const consult = new consultarMTRPorSeuCodigo({
+                seuCodigo: _SEUCODIGO,
+                authToken: _TOKEN,
+                API_BASE_URL: base_url,
+            });
+            const result = consult.getResult();
+
+            const regex = new RegExp(/Unexpected end of JSON input/);
+            await expect(result).rejects.toThrow(regex);
+        });
+    });
+});
