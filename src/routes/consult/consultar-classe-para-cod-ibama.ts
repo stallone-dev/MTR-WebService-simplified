b73ee6af1f3fcecd9b94e8bef1a1017117d21277
stallone-dev <stallone.developer@gmail.com>
2024-09-26 00:52:49 -0300

    refactor(route): Improve 'consultar-casse-para-cod-ibama' route and chance input to object model

--- src/routes/consult/consultar-classe-para-cod-ibama.ts
+++ src/routes/consult/consultar-classe-para-cod-ibama.ts
@@ -11,15 +11,47 @@ import { ApiRequest } from "../../model/api-request.ts";
 
 export { consultarClasseParaCodIBAMA };
 
+/** Interface para implementação */
+interface consultClassePorCodIBAMAConfig {
+    codigoResiduo: string;
+    authToken: MtrWSType.auth.token;
+    API_BASE_URL: MtrWSBaseURL;
+}
+
+/**
+ * Módulo de consulta dos dados de um MTR através do "SeuCodigo"
+ * @SIGOR_ONLY
+ *
+ * @example
+ * ```ts
+ *  import { consultarMtrPorSeuCodigo } from "..."
+ *  import { MtrWSBaseURL } from "..."
+ *
+ *  const token = "Bearer _TOKEN_"
+ *  const base_url = MtrWSBaseURL.SINIR;
+ *  const seu_codigo = { seuCodigo: "1234", suaUnidade: "1234" }
+ *
+ *  // Preparando a API
+ *  const consult = new consultarMtrPorSeuCodigo({
+ *      seuCodigo: seu_codigo,
+ *      authToken: token,
+ *      API_BASE_URL: base_url
+ *  });
+ *
+ *  // Capturando o resultado
+ *  const result = await consult.getResult();
+ *  // ==> { * MTR-object * }
+ * ```
+ */
 class consultarClasseParaCodIBAMA extends ApiRequest {
     private token: MtrWSType.auth.token;
     private residuo_ID: string;
 
-    constructor(
-        codigoResiduo: string,
-        authToken: MtrWSType.auth.token,
-        API_BASE_URL: MtrWSBaseURL,
-    ) {
+    constructor({
+        codigoResiduo,
+        authToken,
+        API_BASE_URL,
+    }: consultClassePorCodIBAMAConfig) {
         super(API_BASE_URL, MtrWSRoute.CONSULTAR_CLASSES_PARA_COD_IBAMA);
         this.token = authToken;
         this.residuo_ID = codigoResiduo;
@@ -35,7 +67,7 @@ class consultarClasseParaCodIBAMA extends ApiRequest {
             MtrWSType.requestModel.consultarClassesParaCodIBAMA,
             MtrWSType.responseModel.consultarClassesParaCodIBAMA
         >({
-            method: "POST",
+            method: "GET",
             pathString: this.residuo_ID,
             auth: this.token,
         });
