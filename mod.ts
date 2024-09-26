2ceefd5adfc87f6019f72892bcf4839290811ff7
stallone-dev <stallone.developer@gmail.com>
2024-09-26 00:55:38 -0300

    feat(project): Add main export namespaces

--- mod.ts
+++ mod.ts
@@ -2,4 +2,7 @@
     This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-// export { * as MtrWS } from "./src/index.ts";
+import { MtrWS } from "./src/_index.ts";
+import * as MtrWSInternals from "@stallone-dev/types-mtr-web-service";
+
+export { MtrWS, MtrWSInternals };
